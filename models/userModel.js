19c5193a82274535dc6f1509e5fa0a8938fe95b1
volk <velvel2k@gmail.com>
2022-05-24 22:25:59 +0300

    login routing

---
+++ models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "dsfasefs$$WT#T#$T#$T$#^%GESG$%U*&^IVSDGRTG$E%";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  date_created: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+exports.UserModel = mongoose.model("users", userSchema);
+
+exports.genToken = (id, email) => {
+  const token = jwt.sign({ id: id, email: email }, JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  return token;
+};
+
+exports.validateUser = (reqBody) => {
+  const joiSchema = Joi.object({
+    name: Joi.string().min(2).max(100).required(),
+    email: Joi.string().min(2).max(25).required().email(),
+    password: Joi.string().min(6).max(25).required(),
+    isAdmin: Joi.boolean(),
+  });
+  return joiSchema.validate(reqBody);
+};
+
+exports.validateLogin = (reqBody) => {
+  const joiSchema = Joi.object({
+    email: Joi.string().min(2).max(25).required().email(),
+    password: Joi.string().min(6).max(25).required(),
+  });
+  return joiSchema.validate(reqBody);
+};
